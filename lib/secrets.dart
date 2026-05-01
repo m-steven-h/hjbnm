47e3726b9b5801c8c7e01041e921914d5dab20ef
STEVEN HANY <steven.hany.194@gmail.com>
2026-05-01 14:54:23 +0300

    Change founderSecretCode value in Secrets class
    Updated founderSecretCode from '2011' to 'steven2011'.

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,8 +1,8 @@
-class Secrets {
+class Secrets {
   static const String binId = '69f15b50856a68218984cc09';
 
   static const String masterKey =
       r'$2a$10$1ZM/KLcdAXwPAfmlUNBKXOasdDe/oFLHfebege3oreyvWht7AUhLS';
 
-  static const String founderSecretCode = '2011';
+  static const String founderSecretCode = 'steven2011';
 }
