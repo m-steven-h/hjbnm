23d4e88c1d70855a80a75953cb37bc511b1cda77
STEVEN HANY <steven.hany.194@gmail.com>
2026-05-01 07:41:44 +0300

    Update secrets.dart

--- lib/secrets.dart
+++ lib/secrets.dart
@@ -1,10 +1,8 @@
-// lib/secrets.dart
-
-class Secrets {
+class Secrets {
   static const String binId = '69f15b50856a68218984cc09';
 
   static const String masterKey =
       r'$2a$10$1ZM/KLcdAXwPAfmlUNBKXOasdDe/oFLHfebege3oreyvWht7AUhLS';
 
-  static const String founderSecretCode = 'admin123';
+  static const String founderSecretCode = 'm steven h';
 }
